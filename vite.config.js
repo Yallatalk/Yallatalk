fc47b9124191ad25cdab82807c2dad432ef68c06
Abdulrahim <yallatalk.team@gmail.com>
2025-04-17 02:23:24 +0300

    fix: final vite config without root

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,11 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  root: '.', // نحدد أن الجذر هو المجلد الحالي
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
+      input: resolve(__dirname, 'index.html'),
     },
   },
 })
