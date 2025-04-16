93eb50c51158e4bb6918fbd87aecc0830f8ee85b
Abdulrahim <yallatalk.team@gmail.com>
2025-04-17 02:54:28 +0300

    fix: use relative path to main.jsx and confirm index.html root

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
+  root: '.', // لازم تحدد الجذر
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: resolve(__dirname, './index.html'),
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
     },
   },
 })
