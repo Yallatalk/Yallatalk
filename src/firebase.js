b1df8cbd6f719ef799bc47fccbab85f8c5b076a0
Abdulrahim <yallatalk.team@gmail.com>
2025-04-17 17:41:24 +0300

    fix: updated firebase config to correct API

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getAnalytics } from 'firebase/analytics';
 
 // ✅ إعدادات Firebase من مشروعك الحالي
 const firebaseConfig = {
-  apiKey: "AIzaSyDYJ9sZCQggVSrv-gvF3nW5ui04mzW4Xc",
+  apiKey: "AIzaSyDYJ9sZCQggqVSrv-gvF3nW5ui04mzW4Xc",
   authDomain: "yallatalk-cfbc2.firebaseapp.com",
   projectId: "yallatalk-cfbc2",
   storageBucket: "yallatalk-cfbc2.appspot.com",
   messagingSenderId: "679777293957",
-  appId: "1:679777293957:web:60e6f567e08ead6c3f3bec",
+  appId: "1:679777293957:web:60e5f567e08ead6c3f3bec",
   measurementId: "G-D6Y8DK55JE"
 };
 
