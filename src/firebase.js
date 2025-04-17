78fa46f72ab9eb05ecf7099dcc158f6fad434d71
Abdulrahim <yallatalk.team@gmail.com>
2025-04-17 05:27:27 +0300

    تحديث جديد

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBw51xYupn3p6LVI6bb2Wn5mqRdWOtSS3w",
-  authDomain: "yallatalk-ec91c.firebaseapp.com",
-  projectId: "yallatalk-ec91c",
-  storageBucket: "yallatalk-ec91c.appspot.com",
-  messagingSenderId: "1068180444046",
-  appId: "1:1068180444046:web:7fbb4b5d1dd82310db0adf",
-  measurementId: "G-WQG5S71HW9"
+  apiKey: "AIzaSyDYJ9sZCQggVSrv-gvF3nW5ui04mzW4Xc",
+  authDomain: "yallatalk-cfbc2.firebaseapp.com",
+  projectId: "yallatalk-cfbc2",
+  storageBucket: "yallatalk-cfbc2.appspot.com",
+  messagingSenderId: "679777293957",
+  appId: "1:679777293957:web:60e6f567e08ead6c3f3bec",
+  measurementId: "G-D6Y8DK55JE"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const storage = getStorage(app);
 export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider(); // ✅ إضافة تسجيل Google
+export const storage = getStorage(app);
+export const googleProvider = new GoogleAuthProvider();
